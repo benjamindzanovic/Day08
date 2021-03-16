ebe815c726c13d02a9a2b8b4f985cfcc1a6e21bf
benjamindzanovic <dzanovicbenjamin@gmail.com>
2021-03-16 02:22:05 +0100

    Paseint with radix

---
+++ ex15/src/index.js
@@ -0,0 +1,10 @@
+function convertStrToInt (str){
+
+    return parseInt(str, 16);
+}
+convertStrToInt("BA");
+convertStrToInt("F1");
+convertStrToInt("JeffBezos");
+console.log(convertStrToInt("BA"));
+console.log(convertStrToInt("F1"));
+console.log(convertStrToInt("JeffBezos"));
